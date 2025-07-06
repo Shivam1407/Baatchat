0e26780f9db4eabc100e7519efe224447508b62c
Shivam1407 <shivam12c.2316@gmail.com>
2025-07-06 12:06:26 +0530

    Initial commit

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,24 @@
+import { StreamChat } from "stream-chat";
+import "dotenv/config"
+
+const apiKey = process.env.STEAM_API_KEY
+const apiSecret = process.env.STEAM_API_SECRET
+
+if(!apiKey || !apiSecret){
+    console.error("Stream API key or Secret is missing");
+}
+ // this is to make connection with stream
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+// upsertStreamUser is used to create a user in stream
+export const upsertStreamUser = async (userData) => {
+    try {
+        await streamClient.upsertUsers([userData]);
+        return userData;
+    } catch (error) {
+        console.error("Error upserting Stream user:", error);
+    }
+}
+// todo: do it later
+//channel banane ke liye use hoga (jaise 1-on-1 ya group chat ke liye).
+export const generateStreamChannel = (userId) => {}
---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import "dotenv/config";
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+const PORT = process.env.PORT
+
+const app = express()
+
+app.use(express.json());
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port 5k1 ${PORT}`);
+    connectDB();
+});
+
